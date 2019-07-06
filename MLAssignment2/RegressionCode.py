dbcabc148fb0c4e05caaf45943880b4956734936
Khalid Riyaz <Khalidriyazgit@gmail.com>
2019-07-06 22:25:31 +0530

    .

--- MLAssignment2/RegressionCode.py
+++ MLAssignment2/RegressionCode.py
@@ -8,7 +8,7 @@ coeffd = []
 intercepts = []
 mmres = []
 for i in range(1,57):
-    file_name = "/home/zahed/Desktop/MLAssignment2/{}.csv".format(i)
+    file_name = "/home/<user_name>/Desktop/MLAssignment2/{}.csv".format(i)
     df = pd.read_csv(file_name, header=None, index_col=False)
     df.dropna(axis=0)
     df.dropna(axis=1)
@@ -31,7 +31,7 @@ for i in range(1,57):
     mres_df = abs(y_preds - y_test)/y_test_mod
     mmres.append(np.mean(mres_df))
     
-    pd.DataFrame(msqs).to_excel(excel_writer="/home/zahed/Desktop/MLAssignment2/MeanSq.xlsx", header=None, index=True)
-    pd.DataFrame(coeffd).to_excel(excel_writer="/home/zahed/Desktop/MLAssignment2/Coefficients.xlsx", header=None, index=True)
-    pd.DataFrame(intercepts).to_excel(excel_writer="/home/zahed/Desktop/MLAssignment2/Intercepts.xlsx", header=None, index=True)
-    pd.DataFrame(mmres).to_excel(excel_writer="/home/zahed/Desktop/MLAssignment2/MeanMagRelError.xlsx", header=None, index=True)
+    pd.DataFrame(msqs).to_excel(excel_writer="/home/<user_name>/Desktop/MLAssignment2/MeanSq.xlsx", header=None, index=True)
+    pd.DataFrame(coeffd).to_excel(excel_writer="/home/<user_name>/Desktop/MLAssignment2/Coefficients.xlsx", header=None, index=True)
+    pd.DataFrame(intercepts).to_excel(excel_writer="/home/<user_name>/Desktop/MLAssignment2/Intercepts.xlsx", header=None, index=True)
+    pd.DataFrame(mmres).to_excel(excel_writer="/home/<user_name>/Desktop/MLAssignment2/MeanMagRelError.xlsx", header=None, index=True)
