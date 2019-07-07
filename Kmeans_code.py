ff0fa5570c045a626d308a5dc727159fa4192756
Khalid Riyaz <Khalidriyazgit@gmail.com>
2019-07-07 10:58:59 +0530

    Update Kmeans_code.py

--- Kmeans_code.py
+++ Kmeans_code.py
@@ -2,11 +2,13 @@ import pandas as pd
 
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score as sc
+from sklearn.metrics import silhouette_samples
 
 dataframes = []
 for i in range(1,57):
-    file_name = "/home/<user_name>/Desktop/python-machine-learning-/{}.csv".format(i)
+    file_name = "/home/<user>/Desktop/python-machine-learning-/{}.csv".format(i)
     df = pd.read_csv(file_name, header=None, index_col=False)
+    df.drop(df.columns[len(df.columns)-1], axis=1, inplace=True)
     df.dropna(axis=0)
     df.dropna(axis=1)
     dataframes.append(df)
@@ -31,8 +33,8 @@ for i in range(0, 56):
 sse_df = pd.DataFrame(sses)
 silhouette_scores_df = pd.DataFrame(silhouette_scores)
 
-sse_df.to_excel(excel_writer="/home/<user_name>/Desktop/python-machine-learning-/sse.xlsx", header=None, index=True)
-silhouette_scores_df.to_excel(excel_writer="/home/<user_name>/Desktop/python-machine-learning-/silhouette_scores.xlsx", header=None, index=True)
+sse_df.to_excel(excel_writer="/home/<user>/Desktop/python-machine-learning-/1_to_20/sse.xlsx", header=None, index=True)
+silhouette_scores_df.to_excel(excel_writer="/home/zahed/Desktop/python-machine-learning-/1_to_20/silhouette_scores.xlsx", header=None, index=True)
 
 # Code for generating silhouette coefficients of individual samples in all datasets.
 for i in range(0, 56):
@@ -40,5 +42,5 @@ for i in range(0, 56):
     labels = kmeans.fit_predict(normalized_dataframes[i])
     coeff = silhouette_samples(normalized_dataframes[i], labels)
     coeff_df = pd.DataFrame(coeff)
-    file_name = "/home/<user_name>/Desktop/python-machine-learning-/Coefficients/silhouette_coeffs_{}.xlsx".format(i+1)
+    file_name = "/home/<user>/Desktop/python-machine-learning-/1_to_20/Coefficients/silhouette_coeffs_{}.xlsx".format(i+1)
     coeff_df.to_excel(excel_writer=file_name, header=None, index=True)
